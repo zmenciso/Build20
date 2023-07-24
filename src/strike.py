28508324a1580ebd09c6b57b92acc41f2a208c9d
Zephan M. Enciso <zmenciso@pm.me>
2023-07-24 13:18:01 -0400

    Strike: Add sneak attack and runes

--- src/strike.py
+++ src/strike.py
@@ -2,8 +2,11 @@ from src.text import write_preamble, fprint, write_cap
 
 
 # TODO: Add support for extra damage (sneak, deadly, etc.)
+# TODO: Deal with crits better
+# TODO: Add support for agile, finesse
 
 RUNES = {
+    '': 1,
     'striking': 2,
     'greaterStriking': 3,
     'majorStriking': 4
@@ -13,6 +16,13 @@ DAMAGE = {
     'S': 'slashing',
     'P': 'piercing',
     'B': 'bludgeoning'
+    }
+
+SNEAK = {
+    0: '[[1d6]] precision',
+    1: '[[2d6]] precision',
+    2: '[[3d6]] precision',
+    3: '[[4d6]] precision'
     }
 
 
@@ -32,4 +42,17 @@ def write_strike(data, modifiers, outfile, header):
         write_cap(outfile, end='')
 
         fprint('{{Damage = ' + damage, header, file=outfile)
+
+        if 'Sneak Attack' in data['specials']:
+            sneak_damage = SNEAK[(data['level'] + 1) // 6]
+            fprint('?{Sneak? | Yes, \n' + sneak_damage + '| No, \nNo sneak attack}',
+                   header, file=outfile)
+
+        if len(item['runes']) > 0:
+            write_cap(outfile, cap='}}', end='')
+
+            fprint('{{Runes = ' + item['runes'][0], header, file=outfile)
+            for rune in item['runes'][1:]:
+                fprint(f'\n{rune}', header, file=outfile)
+
         write_cap(outfile, cap='}}')
